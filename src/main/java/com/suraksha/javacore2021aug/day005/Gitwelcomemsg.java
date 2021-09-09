58b4d9b3f1e25f026cafa26fdf6e1fee99906419
suraksha <dulalsuraksha@gmail.com>
2021-09-09 01:11:33 -0500

    day005 : Git Welcome Msg

---
+++ src/main/java/com/suraksha/javacore2021aug/day005/Gitwelcomemsg.java
@@ -0,0 +1,12 @@
+package com.suraksha.javacore2021aug.day005;
+
+public class Gitwelcomemsg {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to git");
+        System.out.println("This is git from cerotid");
+        System.out.println("WE are starting git class");
+
+    }
+
+}
