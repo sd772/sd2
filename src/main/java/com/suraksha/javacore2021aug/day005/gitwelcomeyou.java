8d3a74363fdd324796ba5466476d9c4a050cd5ea
suraksha <dulalsuraksha@gmail.com>
2021-09-10 17:17:11 -0500

    day004 : Welcome to you

---
+++ src/main/java/com/suraksha/javacore2021aug/day005/gitwelcomeyou.java
@@ -0,0 +1,11 @@
+package com.suraksha.javacore2021aug.day005;
+
+public class gitwelcomeyou {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to all !!");
+        System.out.println("git always cool ");
+
+    }
+}
