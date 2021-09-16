024609999ea1fee637fa88d4f7b4b1f5fb7d554b
suraksha <dulalsuraksha@gmail.com>
2021-09-16 11:07:16 -0500

    day007 : string function

---
+++ src/main/java/com/suraksha/javacore2021aug/day007/stringfunction.java
@@ -0,0 +1,12 @@
+package com.suraksha.javacore2021aug.day007;
+
+public class stringfunction {
+    public static void main(String[] args) {
+        String sampleString = "This is Java 101 Session for ceretid Inc.";
+
+        System.out.println("Sample String : " + sampleString);
+
+
+
+    }
+}
