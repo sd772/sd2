8674a014c7094898a7c442c22c0b8af7bac4e391
suraksha <dulalsuraksha@gmail.com>
2021-09-23 18:03:54 -0500

    day 008 : static funtion

--- src/main/java/com/suraksha/javacore2021aug/day008/StaticFunctionDemo.java
+++ src/main/java/com/suraksha/javacore2021aug/day008/StaticFunctionDemo.java
@@ -6,6 +6,8 @@ public class StaticFunctionDemo {
         System.out.println(mathslibrary.ADDITION(78,34));
         System.out.println(mathslibrary.SUBTRACTION(56,45));
         System.out.println(mathslibrary.MULTIPLICATION(2,4));
-        System.out.println(mathslibrary.DIVISION(12,2)}
+        System.out.println(mathslibrary.DIVISION(12,2));
+    }
+
 
 }
