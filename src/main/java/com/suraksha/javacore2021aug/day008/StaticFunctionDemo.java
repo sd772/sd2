f0711ed5b23f9f838987c0db97f52d6be457ee92
suraksha <dulalsuraksha@gmail.com>
2021-09-22 20:30:55 -0500

    day008 : mathslibrary and static function demo

---
+++ src/main/java/com/suraksha/javacore2021aug/day008/StaticFunctionDemo.java
@@ -0,0 +1,11 @@
+package com.suraksha.javacore2021aug.day008;
+
+public class StaticFunctionDemo {
+
+    public static void main(String[]args) {
+        System.out.println(mathslibrary.ADDITION(78,34));
+        System.out.println(mathslibrary.SUBTRACTION(56,45));
+        System.out.println(mathslibrary.MULTIPLICATION(2,4));
+        System.out.println(mathslibrary.DIVISION(12,2)}
+
+}
---
+++ src/main/java/com/suraksha/javacore2021aug/day008/mathslibrary.java
@@ -0,0 +1,26 @@
+     package com.suraksha.javacore2021aug.day008;
+
+public class mathslibrary {
+
+    public static Integer ADDITION(Integer firstNum, Integer secNum) {
+        System.out.println("2 Integer Addition Static Function");
+        return firstNum + secNum;
+    }
+
+    public static Integer SUBTRACTION(Integer firstNum, Integer secNum) {
+        System.out.println("2 Integer Subtraction Static Function");
+        return firstNum + secNum;
+    }
+
+    public static Integer MULTIPLICATION(Integer firstNum, Integer secNum) {
+        System.out.println("2 Integer Multiplication Static Function");
+        return firstNum + secNum;
+
+    }
+
+    public static Double DIVISION(Integer firstNum, Integer secNum) {
+        System.out.println("2 Integer Division Static Function");
+        return (double)firstNum + secNum;
+    }
+
+}
